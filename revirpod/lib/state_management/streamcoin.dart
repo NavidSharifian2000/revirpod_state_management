18e2b977721d2ab1544f31e6aed096a1e179ac72
NavidSharifian79 <nsharifian0910@gmail.com>
2023-08-17 17:39:08 +0330

    stream by socket io

--- revirpod/lib/state_management/streamcoin.dart
+++ revirpod/lib/state_management/streamcoin.dart
@@ -1,30 +1,66 @@
+import 'dart:async';
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:socket_io_client/socket_io_client.dart' as IO;
 import 'package:socket_io_client/socket_io_client.dart';
 
-class coinstream extends StatelessWidget {
+class coinstream extends ConsumerWidget {
   coinstream({super.key});
-  IO.Socket? socket;
 
   @override
-  Widget build(BuildContext context) {
-    socket = IO.io(
-        'https://api.wallex.ir/',
-        OptionBuilder().setTransports(['websocket']) // for Flutter or Dart VM
-            .build());
-    socket!.onConnect((_) {
-      print('connected');
+  final providerofsocket = StreamProvider.autoDispose((ref) async* {
+    StreamController stream = StreamController();
+    var socket = new SocketService.initial();
+    socket.socket!.on("Broadcaster", (data) {
+      stream.add(data);
     });
-    socket!.onConnectError((data) => {print(data.toString())});
-    socket!.connect();
+
+    //  socket.socket!.on("Broadcaster", (data) {
+    //     log(data);
+    //     stream.add(data);
+    //   });
+    await for (final value in stream.stream) {
+      print(value[1]["timestamp"]);
+      yield value;
+    }
+  });
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    var data2 = ref.watch(providerofsocket);
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
           child: Column(
-            children: [Text("navid")],
+            children: [
+              data2.when(data: (data) {
+                return Text(data[1]["timestamp"].toString());
+              }, error: (error, stacktTrace) {
+                return Text("error");
+              }, loading: () {
+                return CircularProgressIndicator();
+              }),
+              ElevatedButton(onPressed: () {}, child: Text("press"))
+            ],
           ),
         ),
       ),
     );
+  }
+}
+
+class SocketService {
+  IO.Socket? socket;
+  SocketService.initial() {
+    socket = IO.io(
+        'https://api.wallex.ir/',
+        OptionBuilder().setTransports(['websocket']) // for Flutter or Dart VM
+            .build());
+    socket!.connect();
+    socket!.onConnect((data) {
+      print("connected");
+    });
+    socket!.emit('subscribe', {"channel": "USDTTMN@trade"});
   }
 }
